9d9fba95bf8d9d3716c50f1e917ef0d51448eeb8
Fritz <orelawanson@gmail.com>
2025-02-26 09:02:49 +0000

    new changes

--- src/pages/Wallet.jsx
+++ src/pages/Wallet.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { FaArrowUp, FaArrowDown } from "react-icons/fa";
+import { VscPercentage } from "react-icons/vsc";
 import "../pages/Wallet.css";
 import Rectangle4221 from "../assets/Rectangle4221.png";
 import Rectangle4219 from "../assets/Rectangle4219.png";
@@ -13,12 +14,12 @@ const Wallet = ({ onClose }) => {
     const tabs = ['All', 'Loaded Funds', 'Referral Earnings', 'Withdraws'];
 
     const transactions = [
-      { id: 1, reference: "19GUX94836X", amount: 1000.00, description: "Transfer from referral PAYSTACK", date: "16-01-2024", time: "4:30pm", type: "Credit" },
-      { id: 2, reference: "19GUFFI84H5", amount: 20000.00, description: "Loaded Funds from CANARY BLACK", date: "12-01-2024", time: "2:45pm", type: "Credit" },
-      { id: 3, reference: "19GGJGJ7F49", amount: -10000.00, description: "Withdraw to BOND INYANG", date: "12-01-2024", time: "4:30pm", type: "Debit" },
-      { id: 4, reference: "19GGJOSBR01", amount: 17000.00, description: "Loaded Funds from CANARY BLACK", date: "12-01-2024", time: "3:30pm", type: "Credit" },
-      { id: 5, reference: "19GAJF90BF4", amount: 10000.00, description: "Transfer from referral FEMI BANJO", date: "11-01-2024", time: "4:30pm", type: "Credit" },
-      { id: 6, reference: "19GGJKNDKJF", amount: -2500.00, description: "Withdraw to STANLEY UMOH", date: "11-01-2024", time: "2:30pm", type: "Debit" }
+        { id: 1, reference: "19GUX94836X", amount: 1000.00, description: "Transfer from referral PAYSTACK", date: "16-01-2024", time: "4:30pm", type: "Credit" },
+        { id: 2, reference: "19GUFFI84H5", amount: 20000.00, description: "Loaded Funds from CANARY BLACK", date: "12-01-2024", time: "2:45pm", type: "Credit" },
+        { id: 3, reference: "19GGJGJ7F49", amount: -10000.00, description: "Withdraw to BOND INYANG", date: "12-01-2024", time: "4:30pm", type: "Debit" },
+        { id: 4, reference: "19GGJOSBR01", amount: 17000.00, description: "Loaded Funds from CANARY BLACK", date: "12-01-2024", time: "3:30pm", type: "Credit" },
+        { id: 5, reference: "19GAJF90BF4", amount: 10000.00, description: "Transfer from referral FEMI BANJO", date: "11-01-2024", time: "4:30pm", type: "Credit" },
+        { id: 6, reference: "19GGJKNDKJF", amount: -2500.00, description: "Withdraw to STANLEY UMOH", date: "11-01-2024", time: "2:30pm", type: "Debit" }
     ];
 
     const totalCredit = transactions
@@ -26,11 +27,11 @@ const Wallet = ({ onClose }) => {
         .reduce((sum, transaction) => sum + transaction.amount, 0);
 
     const filteredTransactions = activeTab === 'All'
-        ? transactions.filter(transaction => 
-            transaction.description.includes('Transfer from referral') || 
+        ? transactions.filter(transaction =>
+            transaction.description.includes('Transfer from referral') ||
             transaction.description.includes('Withdraw') ||
             transaction.description.includes('Loaded Funds')
-          )
+        )
         : transactions.filter(transaction => {
             if (activeTab === 'Referral Earnings') {
                 return transaction.description.includes('Transfer from referral');
@@ -91,6 +92,7 @@ const Wallet = ({ onClose }) => {
                     ))}
                 </div>
             </div>
+
             <div className="transactions-list">
                 {filteredTransactions.map(transaction => (
                     <div key={transaction.id} className="transaction-item">
@@ -98,7 +100,7 @@ const Wallet = ({ onClose }) => {
                         <p className={`transaction-amount ${transaction.type.toLowerCase()}`}
                             style={{ display: 'flex', alignItems: 'center' }}>
                             <span className="transaction-arrow" style={{
-                                background: transaction.amount < 0 ? '#FF2A23' : '#6CC51D',
+                                background: activeTab === "Referral Earnings" ? 'gold' : (transaction.amount < 0 ? '#FF2A23' : '#6CC51D'),
                                 color: '#000',
                                 borderRadius: '50%',
                                 width: '24px',
@@ -108,13 +110,15 @@ const Wallet = ({ onClose }) => {
                                 justifyContent: 'center',
                                 marginRight: '8px'
                             }}>
-                                {transaction.amount < 0 ? <FaArrowUp style={{ fontSize: '14px' }} /> : <FaArrowDown style={{ fontSize: '14px' }} />}
+                                {transaction.amount < 0 ? <FaArrowUp style={{ fontSize: '14px' }} /> :
+                                    activeTab === "Referral Earnings" ? <VscPercentage style={{ fontSize: '14px' }} /> :
+                                        <FaArrowDown style={{ fontSize: '14px' }} />}
                             </span>
                             ₦{Math.abs(transaction.amount).toLocaleString()}
                         </p>
                         <p className="transaction-description">{transaction.description} on {transaction.date} || {transaction.time}</p>
                         <span className={`transaction-type ${transaction.type.toLowerCase()}`}>{transaction.type}</span>
-                        <button className="transaction-download"><MdOutlineFileDownload className="Mdoutlinefiledownload"/> Download</button>
+                        <button className="transaction-download"><MdOutlineFileDownload className="Mdoutlinefiledownload" /> Download</button>
                     </div>
                 ))}
             </div>
