b832ee20f7be5fbb20e67d60fce90d3f6f40b7ba
Fritz <orelawanson@gmail.com>
2025-04-16 17:38:14 +0100

    new changes

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,18 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
   build: {
-    chunkSizeWarningLimit: 3000, // Allow up to 3MB chunks
+    chunkSizeWarningLimit: 3000, // 3MB chunks
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          react: ['react', 'react-dom'],
+          vendor: ['lodash', 'axios'], // Add heavy libraries here
+        }
+      }
+    }
   },
 })
