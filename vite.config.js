19d8d09bde1b9413579bf39dc17ae575bcdd582d
Fritz <orelawanson@gmail.com>
2025-04-17 10:44:22 +0100

    new changes

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,15 @@
-// vite.config.js
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import chunkSplitPlugin from 'vite-plugin-chunk-split';
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    chunkSplitPlugin({
+      strategy: 'all-in-one', // Customize strategy, or try 'single-vendor'
+    }),
+  ],
   build: {
-    chunkSizeWarningLimit: 3000, // 3MB chunks
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          react: ['react', 'react-dom'],
-          vendor: ['lodash', 'axios'], // Add heavy libraries here
-        }
-      }
-    }
+    chunkSizeWarningLimit: 3000, // 3MB
   },
-})
+});
